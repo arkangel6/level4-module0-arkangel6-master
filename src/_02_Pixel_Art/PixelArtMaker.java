77e6ca59854446211db4a2419537f41e400efe36
11000783 <josephaguilar2022@gmail.com>
2018-11-12 19:59:57 -0800

    11/12/18

--- src/_02_Pixel_Art/PixelArtMaker.java
+++ src/_02_Pixel_Art/PixelArtMaker.java
@@ -1,5 +1,6 @@
 package _02_Pixel_Art;
 
+import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -108,4 +109,13 @@ public class PixelArtMaker implements MouseListener, ActionListener{
 		
 		
 	}
+	
+	public static Color hex2Rgb(String colorStr) {
+	    return new Color(
+	            Integer.valueOf( colorStr.substring( 1, 3 ), 16 ),
+	            Integer.valueOf( colorStr.substring( 3, 5 ), 16 ),
+	            Integer.valueOf( colorStr.substring( 5, 7 ), 16 ) );
+	}
+	
+	
 }
